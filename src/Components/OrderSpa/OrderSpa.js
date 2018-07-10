bacfed057f235826af9aff0b0a5573d5bfcb10eb
Andy <amillion3@gmail.com>
2018-07-09 21:01:43 -0500

    added order page functionality and a Link to the new order page

--- src/Components/OrderSpa/OrderSpa.js
+++ src/Components/OrderSpa/OrderSpa.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import moment from 'moment';
+
+import orderRequests from '../../firebaseRequests/orders';
+import authRequests from '../../firebaseRequests/auth';
+
 import './OrderSpa.css';
 
 class OrderSpa extends React.Component {
+  state = {
+    orders: [],
+  };
+
+  componentDidMount () {
+    orderRequests
+      .getRequest(authRequests.getUid())
+      .then(orders => {
+        this.setState({orders: orders});
+        // this.setState({orders});  ES6 shorthand
+      })
+      .catch(err => {
+        console.error('error with orders get request ', err);
+      });
+  }
+
   render () {
+    const orderComponents = this.state.orders.map(order => {
+      return (
+        <button
+          key={order.id}
+          className='col-xs-12 btn btn-default order-button'
+        >
+          <span className='col-xs-6'>Order number: {order.id}</span>
+          <span className='col-xs-6'>Order date: {moment(order.dateTime).format('LLL')}</span>
+        </button>
+      );
+    });
     return (
-      <div className='OrderSpa'>
-        <h1>OrderSpa</h1>
+      <div className='OrderSpa col-xs-12'>
+        <h2>Orders</h2>
+        <button>
+          <Link to='/new'>
+            New Order
+          </Link>
+        </button>
+        <ul>
+          {orderComponents}
+        </ul>
       </div>
     );
   }
