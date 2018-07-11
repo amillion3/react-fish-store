a2e882ffd764b9480e068c799777a3e316d7b1a2
Andy <amillion3@gmail.com>
2018-07-10 20:23:30 -0500

    delete from firebase functionality

--- src/Components/SingleOrder/SingleOrder.js
+++ src/Components/SingleOrder/SingleOrder.js
@@ -1,11 +1,27 @@
 import React from 'react';
+
+import orderRequests from '../../firebaseRequests/orders';
+
 import './SingleOrder.css';
 
 class SingleOrder extends React.Component {
+
+  deleteOrderClick = () => {
+    const firebaseId = this.props.match.params.id;
+    orderRequests
+      .deleteRequest(firebaseId)
+      .then(() => {
+        this.props.history.push('./orders');
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
   render () {
     return (
       <div className='SingleOrder'>
         <h1>Single Order</h1>
+        <button className='btn btn-danger' onClick={this.deleteOrderClick}>Delete Order</button>
       </div>
     );
   }
